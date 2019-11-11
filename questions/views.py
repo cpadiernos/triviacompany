8625a06c5514a6c916dd431fb9e330df212d9f5a
Carol Padiernos <cpadiernos@gmail.com>
2019-11-11 20:44:29 +0000

    Update for email service and protected file serve

--- questions/views.py
+++ questions/views.py
@@ -4,7 +4,7 @@ import os
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.views import generic
 from django.views.static import serve
 
@@ -25,4 +25,11 @@ def login_required_private_file(request, path):
     full_path = os.path.join(settings.PRIVATE_STORAGE_ROOT, path)
     if not os.path.isfile(full_path):
         raise Http404
-    return serve(request, path, settings.PRIVATE_STORAGE_ROOT)
+
+    if settings.PRIVATE_MEDIA_USE_XSENDFILE:
+        response = HttpResponse()
+        response['X-Accel-Redirect'] = '/protected/' +  '{0}/{1}'.format(settings.PRIVATE_STORAGE_URL[1:-1], path) # Using Nginx to serve
+        del response['Content-Type']
+        return response
+    else:
+        return serve(request, path, settings.PRIVATE_STORAGE_ROOT)
